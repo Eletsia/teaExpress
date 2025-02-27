e22b05b087ccffc91a233410c7b88f68cdaf0271
Young <dndud5182@gmail.com>
2025-02-27 12:35:27 +0900

    Add : Profile 파일 생성 후 Route 연결 완료

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "../pages/Profile";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/posts" element={<Post />} />
-        <Route path="/posts-create" element={<CreatePost />} />
-        <Route path="/posts-modify" element={<ModifyPost />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/sign-up" element={<SignUp />} /> */}
+        {/* <Route path="/posts" element={<Post />} /> */}
+        {/* <Route path="/posts-create" element={<CreatePost />} /> */}
+        {/* <Route path="/posts-modify" element={<ModifyPost />} /> */}
         <Route path="/profile" element={<Profile />} />
-        <Route path="/results" element={<TestResult />} />
+        {/* <Route path="/results" element={<TestResult />} /> */}
       </Routes>
     </BrowserRouter>
   );
