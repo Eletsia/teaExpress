f8a8db3f5b101534783e8195827e9bccf6934e0f
ick-web <yih000098@gmail.com>
2025-02-27 20:49:16 +0900

    add: 코드수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import ModifyPost from "./pages/ModifyPost";
 import Router from "./shared/Router";
+import "./index.css"
+
 
 const App = () => {
-  return <Router />;
+  // return <Router />;
+  return <ModifyPost />;
 };
 
 export default App;
