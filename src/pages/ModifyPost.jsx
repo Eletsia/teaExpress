5e76b7844015d3ca9517f9311312e1da00c96a4e
ick-web <yih000098@gmail.com>
2025-02-27 17:03:42 +0900

    move: page -> ModifyPost 이름변경

--- src/pages/ModifyPost.jsx
+++ src/pages/ModifyPost.jsx
@@ -1,28 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import supabase from "../shared/supabase";
 
 const ModifyPost = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [image, setImage] = useState(null);
+  const [users, setUsers] = useState([])
+
+
+  // 유저 데이터 가져오기
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("users").select("*");
+      if (error) {
+        console.log("error => ", error);
+      } else {
+        console.log("data => ", data);
+        setUsers(data);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // 게시물 등록 핸들러
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    //  업로드된 이미지 URL 가져오기
+
+    // 게시물 데이터 삽입
+    const { data, error } = await supabase.from("posts").insert({
+      title: title,
+      content: content,
+      img_list: imageUrl,
+      uid: uid, // 현재 로그인한 사용자 ID 추가
+    });
+    if (error) {
+      console.error("피드 등록 실패:", error);
+      return;
+    }
+  };
+
   return (
-    <div className="flex flex-row">
-      <div className="flex flex-col">
-        <img src="" alt="이미지" />
-        <button>게시물 수정</button>
+    <form className="flex flex-col gap-4" onSubmit={onSubmitHandler}>
+      <h3>게시물 수정페이지</h3>
+      {image && (
+        <img
+          src={image}
+          alt="preview"
+          style={{ width: "200px", height: "auto" }}
+        />
+      )}
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => setImage(e.target.files[0])}
+      />
+      <label>
+        제목
+        <input
+          type="text"
+          value={title}
+          placeholder="제목을 입력하세요."
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </label>
+      <label>
+        내용
+        <input
+          type="text"
+          value={content}
+          placeholder="내용을 입력하세요."
+          onChange={(e) => setContent(e.target.value)}
+        />
+      </label>
+      <div>
+        <button type="submit">등록하기</button>
+        <button type="button">뒤로가기</button>
       </div>
-      <div className="flex flex-col">
-        <div className="flex flex-row justify-between">
-          <h3>제목</h3>
-          <div className="flex flex-row gap-3">
-              <button>좋아요</button>
-              <button>북마크</button>
-          </div>
-        </div>
-        <ul>
-          <li>댓글 1</li>
-          <li>댓글 2</li>
-          <li>댓글 3</li>
-        </ul>
-        <input type="text" name="comment" placeholder="댓글을 입력하세요" />
-      </div>
-    </div>
+    </form>
   );
 };
 
