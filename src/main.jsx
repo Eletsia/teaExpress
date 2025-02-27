a7c4abde6cb467229893d0fdc62b98fa210a78c4
ick-web <yih000098@gmail.com>
2025-02-27 23:29:44 +0900

    fix: Queryprovider 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const queryClient = new QueryClient();
 
