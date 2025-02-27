2856cef4010de27e23ab3ba637d8f05f451d3369
Eletsia <minjh2749@gmail.com>
2025-02-27 19:56:43 +0900

    add: user,comment,like,post,bookMark관련 api

---
+++ src/api/userApi.js
@@ -0,0 +1,49 @@
+import supabase from "../shared/supabase";
+
+// @param id === 유저의 uid값
+// supabase로부터 public.user테이블의 email nickname introduction avatar_img값을 가져옵니다.
+// @return 해당 정보가 담긴 Type : object 의 data 배열열
+export const getUserInfo = async id => {
+  try {
+    const userId = id;
+    if (!userId) throw new Error("잘못된 유저 정보입니다.");
+    const { data, error } = await supabase
+      .from("users")
+      .select("email,nickname,introduction,avatar_img")
+      .eq("uid", userId);
+
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.error("유저 정보 가져오기 오류", error);
+    return null;
+  }
+};
+
+//@param formData 업데이트할 유저의 정보가 담긴 객체
+//@param id 업데이트할 유저의 uid값
+//회원 정보를 수정합니다
+//@return 업데이트한 유저정보를 리턴합니다
+//실패시 null을 리턴합니다.
+export const updataUserInfo = async (formData, id) => {
+  try {
+    const userId = id;
+    if (!userId) throw new Error("잘못된 유저 정보입니다.");
+
+    const { data, error } = await supabase
+      .from("users")
+      .update({
+        nickname: formData.nickname,
+        introduction: formData.inroduction,
+        avatar_img: formData.avatar_img,
+      })
+      .eq("uid", userId)
+      .select();
+
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.error("유저 정보 업데이트 오류", error);
+    return null;
+  }
+};
