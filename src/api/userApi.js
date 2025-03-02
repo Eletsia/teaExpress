e4d80d643556833685ac69d3ab7cf0639fe114c5
Young <dndud5182@gmail.com>
2025-03-02 18:10:17 +0900

    Docs : 띄어쓰기 및 코드 줄바꿈으로 인한 가독성 향상

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -2,7 +2,7 @@ import supabase from "../shared/supabase";
 
 // @param id === 유저의 uid값
 // supabase로부터 public.user테이블의 "email" "nickname" "introduction" "avatar_img" 값을 가져옵니다.
-// @return 해당 정보가 담긴 Type : object 의 data 배열
+// @return 해당 정보가 담긴 Type : object의 data배열
 export const getUserInfo = async id => {
   try {
     const userId = id;
@@ -19,14 +19,15 @@ export const getUserInfo = async id => {
     return data;
   } catch (error) {
     console.error("유저 정보 가져오기 오류", error);
+
     return null;
   }
 };
 
 // @param formData 업데이트할 유저의 정보가 담긴 객체
 // @param id 업데이트할 유저의 uid값
-// 회원 정보를 수정합니다
-// @return 업데이트한 유저 정보를 리턴합니다
+// 회원 정보를 수정합니다.
+// @return 업데이트한 유저 정보를 리턴합니다.
 // 실패 시, null을 리턴합니다.
 export const updataUserInfo = async (formData, id) => {
   try {
@@ -49,6 +50,7 @@ export const updataUserInfo = async (formData, id) => {
     return data;
   } catch (error) {
     console.error("유저 정보 업데이트 오류", error);
+
     return null;
   }
 };
