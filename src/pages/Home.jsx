ae378bf00e40931065034de5eda43c13c90eefc2
Young <dndud5182@gmail.com>
2025-03-03 23:45:30 +0900

    Style : 공통 버튼 스타일 적용 및 일부 스타일 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="flex h-screen w-screen flex-col">
       <Header />
-      <div className="flex flex-1 items-center justify-center bg-gray-500">
+      <div className="flex flex-1 items-center justify-center bg-[#E0F2F1]">
         콘텐츠 영역
       </div>
       <Footer />
