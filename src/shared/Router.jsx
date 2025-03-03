96de05de93ecfc29dd5ede666f2d3f4e060b9260
Young <dndud5182@gmail.com>
2025-03-03 22:06:56 +0900

    Feat : 프로필 정보 수정 및 업데이트 기능 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -7,13 +7,13 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/posts" element={<Post />} />
         <Route path="/posts-create" element={<CreatePost />} />
         <Route path="/posts-modify" element={<ModifyPost />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/results" element={<TestResult />} />  */}
+        <Route path="/results" element={<TestResult />} />
       </Routes>
     </BrowserRouter>
   );
