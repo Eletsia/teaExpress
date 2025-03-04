08e6b81885ae947f78508d063630d2716fd87c3b
ick-web <yih000098@gmail.com>
2025-03-04 14:30:09 +0900

    update: 좋아요,북마크 기능 추가

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,8 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { deleteBookMark, getBookMark, insertBookMark } from "../api/bookMarkApi";
 import { getComments, insertComment } from "../api/commentApi";
 import { loadFile } from "../api/imgApi";
+import { deleteLike, getLike, insertLike } from "../api/likeApi";
 import { getPostById } from "../api/postApi";
 import { getUserInfo } from "../api/userApi";
 import supabase from "../shared/supabase";
@@ -19,6 +21,8 @@ const Post = () => {
   const { id } = useParams();
   const [comment, setComment] = useState("");
   const navigate = useNavigate();
+  const [isLiked, setIsLiked] = useState(false);
+  const [isBooked, setIsBooked] = useState(false);
 
 
   //게시물 정보 가져오기
@@ -72,6 +76,36 @@ const Post = () => {
     },
   });
 
+  //좋아요 추가
+  const likeInsertMutation = useMutation({
+    mutationFn: ({uid, id}) => insertLike(uid, id),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["like"]);
+    },
+  });
+  //좋아요 삭제
+  const likeDeleteMutation = useMutation({
+    mutationFn: ({uid, id}) => deleteLike(uid, id),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["like"]);
+    },
+  });
+
+  //북마크 추가
+  const bookMarkInsertMutation = useMutation({
+    mutationFn: ({uid, id}) => insertBookMark(uid, id),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["like"]);
+    },
+  });
+  //북마크 삭제
+  const bookMarkDeleteMutation = useMutation({
+    mutationFn: ({uid, id}) => deleteBookMark(uid, id),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["like"]);
+    },
+  });
+
   // 이미지 관리
   const {
     data: imageUrl,
@@ -86,7 +120,27 @@ const Post = () => {
     },
   });
 
-  if (isPostLoading || isCommentsLoading || isImageLoading || isUserLoading) {
+  // 좋아요 정보 가져오기
+  const {
+    data: like,
+    isLoading: isLikeLoading,
+    isError: isLikeError,
+  } = useQuery({
+    queryKey: ["like", data.user?.id,id],
+    queryFn: () => getLike(data.user?.id,id),
+  });
+
+  // 북마크 정보 가져오기
+  const {
+    data: bookmarked,
+    isLoading: isBookedLoading,
+    isError: isBookedError,
+  } = useQuery({
+    queryKey: ["bookmarked", data.user?.id,id],
+    queryFn: () => getBookMark(data.user?.id,id),
+  });
+
+  if (isPostLoading || isCommentsLoading || isImageLoading || isUserLoading || isLikeLoading || isBookedLoading) {
     return <div>로딩 중입니다...</div>;
   }
 
@@ -104,6 +158,14 @@ const Post = () => {
 
   if (isUserError) {
     console.error("유저정보를 불러오는 중 오류 발생");
+  }
+
+  if (isLikeError) {
+    console.error("좋아요 정보를 불러오는 중 오류 발생");
+  }
+
+  if (isBookedError) {
+    console.error("좋아요 정보를 불러오는 중 오류 발생");
   }
 
 
@@ -127,6 +189,17 @@ const Post = () => {
 
   };
 
+  //좋아요 ON,OFF
+  const likeToggleButton = () => {
+    setIsLiked(prevState => !prevState);
+    isLiked ? likeDeleteMutation.mutate({uid: data.user?.id,id:id}) : likeInsertMutation.mutate({uid: data.user?.id,id:id})
+    // console.log("uid:",data.user?.id)
+  }
+
+  const bookMarkToggleButton = () =>{
+    setIsBooked(prevState => !prevState)
+    isBooked ? bookMarkDeleteMutation.mutate({uid: data.user?.id,id:id}): bookMarkInsertMutation.mutate({uid: data.user?.id,id:id})
+  }
 
 
   return (
@@ -134,13 +207,13 @@ const Post = () => {
       {/* 왼쪽 */}
       <div className="bg-red-300 flex flex-col items-center gap-4">
         <img src= {imageUrl || ""}  alt="image" className="w-60 h-auto rounded-lg shadow" />
-        <p>닉네임: {user[0].nickname}</p>
+        <p>닉네임: {user?.[0]?.nickname}</p>
         <div className="bg-green-300 flex flex-row justify-between p-4">
-          <p className="p-2 text-md">{post[0].location || "위치 정보 없음"}</p>
+          <p className="p-2 text-md">{post?.[0]?.location || "위치 정보 없음"}</p>
           <p className="p-2 text-md">거리: </p>
         </div>
         <div className="bg-orange-300 p-4">
-          <p className="p-2 text-md">{post[0].content || "내용 없음"}</p>
+          <p className="p-2 text-md">{post?.[0]?.content || "내용 없음"}</p>
         </div>
         {/* 로그인한 유저의 게시물이면 게시물 수정하기 버튼 나오게 */}
         {data.user.id === post?.[0]?.uid ? <button onClick={() => navigate(`/posts-modify/${id}`)} className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition">
@@ -152,8 +225,8 @@ const Post = () => {
         <div className="bg-violet-300 flex flex-row items-center justify-between p-4">
           <p className="p-2 text-2xl">{post?.[0]?.title || "제목 없음"}</p>
           <div className="flex gap-2">
-            <button>좋아요</button>
-            <button>북마크</button>
+            <button onClick={likeToggleButton}>{isLiked ? "on":"off"} {like.length}</button>
+            <button onClick={bookMarkToggleButton}>{isBooked ? "북마크" : "북마크 off"}</button>
           </div>
         </div>
         
