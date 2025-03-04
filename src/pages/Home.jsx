1c236c767eb9f7d63f531ec7298a914fef1d0588
answq <wnffldksla@gmail.com>
2025-03-04 12:07:03 +0900

    add : kakaoAPI 파일 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { KakaoMap } from "../api/kakaoAPI";
 
 const Home = () => {
   return (
     <div className="flex h-screen w-screen flex-col">
       <Header />
-      <div className="flex-center flex-1 bg-[#E0F2F1]">콘텐츠 영역</div>
+      <div className="flex-center flex-1 bg-[#E0F2F1]"><KakaoMap /></div>
       <Footer />
     </div>
   );
