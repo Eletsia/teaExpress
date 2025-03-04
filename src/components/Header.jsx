88883b2721e7a7f80c2566bbb4cf0e1cdc4c9559
Young <dndud5182@gmail.com>
2025-03-04 17:23:43 +0900

    Style : css 속성 [ w-full ] 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   const navigate = useNavigate();
 
   return (
-    <header className="flex items-center justify-between bg-[#80cbc4] p-6">
+    <header className="flex w-full items-center justify-between bg-[#80cbc4] p-6">
       <h1 className="text-xl font-bold">Tea Express</h1>
       <div className="flex gap-2">
         <button onClick={() => navigate("/login")}>로그인</button>
