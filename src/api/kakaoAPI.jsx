94cbbd5b4aaa4d34d0750555643b549766f8b12e
Eletsia <minjh2749@gmail.com>
2025-03-04 18:53:21 +0900

    fix : confilict

--- src/api/kakaoAPI.jsx
+++ src/api/kakaoAPI.jsx
@@ -4,9 +4,9 @@ import { MapMarker } from "react-kakao-maps-sdk";
 // useKakaoLoader 훅을 사용하여 Kakao Maps SDK 로드
 const useKakaoLoader = () => {
   const [kakaoLoaded, setKakaoLoaded] = useState(false);
+
   useEffect(() => {
     if (window.kakao) {
-      console.log(window.kakao);
       setKakaoLoaded(true);
       return;
     }
@@ -15,11 +15,13 @@ const useKakaoLoader = () => {
     script.src =
       "https://dapi.kakao.com/v2/maps/sdk.js?appkey=44f25cb020400a3651519041b51ac8e8&libraries=services,clusterer&autoload=false";
     script.async = true;
+
     //SDK 로드하면 실행
     script.onload = () => {
       setKakaoLoaded(true);
       console.log("Kakao Maps SDK 로드 완료");
     };
+
     //SDK 로드 실패했을 때
     script.onerror = error => {
       console.error("Kakao Maps SDK 로드 실패", error);
@@ -28,19 +30,32 @@ const useKakaoLoader = () => {
   }, []);
   return kakaoLoaded;
 };
-export const KakaoMap = () => {
+export const KakaoMap = ({ posts }) => {
   const kakaoLoaded = useKakaoLoader();
+  const [position, setPosition] = useState({ lat: 33.5563, lng: 126.79581 });
   if (!kakaoLoaded) {
     return <div>Loading Kakao Map...</div>;
   }
+
   return (
-    <Map
-      center={{ lat: 33.5563, lng: 126.79581 }}
-      style={{ width: "100%", height: "360px" }}
-    >
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: "#000" }}>Hello World!</div>
-      </MapMarker>
-    </Map>
+    <>
+      <Map center={position} style={{ width: "100%", height: "360px" }}>
+        <MapMarker
+          position={{
+            lat: 33.5563,
+            lng: 126.79581,
+          }}
+        >
+          <div style={{ color: "#000" }}>qwe</div>
+        </MapMarker>
+      </Map>
+      <p>
+        <em>지도를 클릭해주세요!</em>
+      </p>
+      <div id="clickLatlng">
+        {position &&
+          `클릭한 위치의 위도는 ${position.lat} 이고, 경도는 ${position.lng} 입니다`}
+      </div>
+    </>
   );
 };
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,14 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 import Login from "../pages/Login";
-import SignUp from "../pages/Signup";
-import ModifyPost from "../pages/ModifyPost";
+import Signup from "../pages/Signup";
 import Post from "../pages/Post";
-import Profile from "../pages/Profile";
-import Login from "../pages/Login";
-import SignUp from "../pages/Signup";
 import ModifyPost from "../pages/ModifyPost";
-import Post from "../pages/Post";
 
 const Router = () => {
   return (
@@ -19,7 +14,7 @@ const Router = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/posts/:id" element={<Post />} />
-        <Route path="/posts-create" element={<CreatePost />} />
+        {/* <Route path="/posts-create" element={<CreatePost />} /> */}
         <Route path="/posts-modify/:id" element={<ModifyPost />} />
         <Route path="/profile" element={<Profile />} />
         {/* <Route path="/results" element={<TestResult />} /> */}
