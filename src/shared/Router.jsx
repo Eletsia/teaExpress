b1b706306e655324fb5f46d98be6c1e9339c831d
Eletsia <minjh2749@gmail.com>
2025-03-05 09:26:06 +0900

    add: mapMarker 표시 , 해당 정보기반 포스트 생성성

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -10,6 +10,7 @@ import Layout from "./Layout";
 import { loginUseAuth } from "../store/loginStore";
 import { useEffect } from "react";
 import supabase from "./supabase";
+import PrivateRoute from "../components/PrivateRoute";
 
 const Router = () => {
   const { setUser } = loginUseAuth();
@@ -33,10 +34,22 @@ const Router = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/posts/:id" element={<Post />} />
-          <Route path="/posts-create/:address" element={<CreatePost />} />
-          <Route path="/posts-modify/:id" element={<ModifyPost />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/posts/:id"
+            element={<PrivateRoute element={<Post />} />}
+          />
+          <Route
+            path="/posts-create/:address/:lat/:lng"
+            element={<PrivateRoute element={<CreatePost />} />}
+          />
+          <Route
+            path="/posts-modify/:id"
+            element={<PrivateRoute element={<ModifyPost />} />}
+          />
+          <Route
+            path="/profile"
+            element={<PrivateRoute element={<Profile />} />}
+          />
           {/* <Route path="/results" element={<TestResult />} /> */}
         </Route>
       </Routes>
